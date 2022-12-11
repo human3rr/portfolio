31402e8479519a02098adf9dee13729315ba5ec6
Noah Brewer <noah.t.brewer@gmail.com>
2022-12-10 18:10:12 -0600

    diag box working much better

--- index.js
+++ index.js
@@ -5,32 +5,57 @@
 
 let textPrompts = [
     'Welcome weary traveler...',
-    "It's unexpected to receive visiters this time of year"
+    "It's unexpected to receive visiters this time of year.",
+    "Ah. But nonetheless it's a welcome visit.",
+    "What may I do for you?"
 ]
 
 let dialogBox = document.querySelector('.dialogBox')
 dialogBox.innerHTML = ""
 dialogBox.style.backgroundColor = "#0B0B12";   
 dialogBox.style.height = "50px";   
-dialogBox.style.width = "200px";   
+dialogBox.style.width = "400px";   
 dialogBox.style.color = "#cecee0";   
 
-function changeInnerText(innerText, delay){
+function changeInnerText(innerText, delay)
+{
     console.log({innerText,delay})
-    setTimeout(() => dialogBox.innerHTML = innerText, delay)
+
+    return (new Promise(resolve => {
+        setTimeout(() => {
+            dialogBox.innerHTML = innerText;
+            resolve("resolved")
+        }, delay)
+    }))
 }
-function typeOutText(dialog, timeForCharacters){
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function typeOutText(dialog){
+    await timeout(1000)
+    dialogBox.innerHTML = ""
     let tempDialog = ""
     let i = 100
     for(let c of dialog){
         tempDialog = tempDialog + c
-        changeInnerText(tempDialog, i)
-        i = i + 100
+        await changeInnerText(tempDialog, i)
     }
+    return 1;
 }
 
-typeOutText("Welcome weary traveler...", null)
-typeOutText("It's unexpected to receive visiters this time of year", null)
+ async function typeOutMultiDialogs(diagArry){
+    for (const diag of diagArry) {
+        console.log(diag)
+        await typeOutText(diag);
+      }
+}
+typeOutMultiDialogs(textPrompts)
+//wrap this call in another function.
+//After dialog is finished, create response options
+
+//typeOutText("Welcome weary traveler...")
+//typeOutText("It's unexpected to receive visiters this time of year", null)
 //setTimeout(() => dialogBox.innerHTML = "world", 1000)
 /*dialog = "hello"
 for(let c of dialog){
