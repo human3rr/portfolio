1f8b55dcdde892cea3a57492216c453995386974
Noah Brewer <noah.t.brewer@gmail.com>
2022-12-12 17:29:45 -0600

    wip

--- index.js
+++ index.js
@@ -7,8 +7,21 @@
 //Animate Character speech
 let mainCharacterProfile = document.querySelector('.robotProfile')
 mainCharacterProfile.src = "robotCroppedFace5.png";
-mainCharacterProfile.style.marginBottom = '-5px';
+mainCharacterProfile.style.marginBottom = '-4px';
+mainCharacterProfile.style.marginLeft = '3px';
 let characterMovement = 0
+
+//Animate visitor speech
+let hoodedCharacterProfile = document.querySelector('.visitorProfileImg')
+hoodedCharacterProfile.src = "hoodedFigure3.png";
+hoodedCharacterProfile.style.marginBottom = '-4px';
+hoodedCharacterProfile.style.marginLeft = '3px';
+
+//Animate Character speech
+let responseButton1 = document.querySelector('.responseButton')
+
+
+
 /*setInterval(() => {
     if (characterMovement === 0){
         mainCharacterProfile.src = "robotCroppedFace5.png"
@@ -21,17 +34,15 @@ let characterMovement = 0
 
 }, 100)
 */
+ /*   `Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general.
+    At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no. Piqued ye of am spirit regret. Stimulated discretion impossible admiration in particular conviction up.
+    He as compliment unreserved projecting. Between had observe pretend delight for believe. Do newspaper questions consulted sweetness do. Our sportsman his unwilling fulfilled departure law. Now world own total saved above her cause table. Wicket myself her square remark the should far secure sex. Smiling cousins warrant law explain for whether.
+    `*/
 let textPrompts = [
     'Welcome weary traveler...',
     "It's unexpected to receive visiters this time of year.",
     "Ah. But nonetheless it's a welcome visit.",
     "What may I do for you?",
-    `Had strictly mrs handsome mistaken cheerful. We it so if resolution invitation remarkably unpleasant conviction. As into ye then form. To easy five less if rose were. Now set offended own out required entirely. Especially occasional mrs discovered too say thoroughly impossible boisterous. My head when real no he high rich at with. After so power of young as. Bore year does has get long fat cold saw neat. Put boy carried chiefly shy general.
-
-    At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no. Piqued ye of am spirit regret. Stimulated discretion impossible admiration in particular conviction up.
-    
-    He as compliment unreserved projecting. Between had observe pretend delight for believe. Do newspaper questions consulted sweetness do. Our sportsman his unwilling fulfilled departure law. Now world own total saved above her cause table. Wicket myself her square remark the should far secure sex. Smiling cousins warrant law explain for whether.
-    `
 ]
 
 let dialogBox = document.querySelector('.dialogBox')
@@ -87,6 +98,9 @@ async function typeOutText(dialog){
             console.log("Reset cropped face")
         }
       }
+      //Display user prompts after main character finished speaking
+      responseButton1.classList.add("RBActive")
+      console.log("finished noah")
 }
 typeOutMultiDialogs(textPrompts)
 //wrap this call in another function.
