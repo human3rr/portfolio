b4bd888b968a04245783b78705daf7cb9f79d5cc
Noah Brewer <noah.t.brewer@gmail.com>
2022-12-15 11:07:20 -0600

    checkpoint

--- index.js
+++ index.js
@@ -23,23 +23,14 @@ setTimeout(() => {
     robotSection.classList.add("RSActive")
 }, 1000)
 
-//Animate Character speech
-let mainCharacterProfile = document.querySelector('.robotProfile')
-mainCharacterProfile.src = "robotPx.png";
-mainCharacterProfile.style.marginBottom = '-4px';
-mainCharacterProfile.style.marginLeft = '3px';
-let characterMovement = 0
-
-//Animate visitor speech
-let hoodedCharacterProfile = document.querySelector('.visitorProfileImg')
-hoodedCharacterProfile.src = "visitorPx.png";
-hoodedCharacterProfile.style.marginBottom = '-4px';
-hoodedCharacterProfile.style.marginLeft = '3px';
-
-//Animate Character speech
 let responseButton1 = document.querySelector('.responseButton')
 
-
+function CharacterProfile(documentClassName, filename){
+    this.characterProfile = document.querySelector(documentClassName)
+    this.characterProfile.src = filename
+    this.characterProfile.style.marginBottom = '-4px';
+    this.characterProfile.style.marginLeft = '3px';
+}
 
 /*setInterval(() => {
     if (characterMovement === 0){
@@ -57,39 +48,46 @@ let responseButton1 = document.querySelector('.responseButton')
     At as in understood an remarkably solicitude. Mean them very seen she she. Use totally written the observe pressed justice. Instantly cordially far intention recommend estimable yet her his. Ladies stairs enough esteem add fat all enable. Needed its design number winter see. Oh be me sure wise sons no. Piqued ye of am spirit regret. Stimulated discretion impossible admiration in particular conviction up.
     He as compliment unreserved projecting. Between had observe pretend delight for believe. Do newspaper questions consulted sweetness do. Our sportsman his unwilling fulfilled departure law. Now world own total saved above her cause table. Wicket myself her square remark the should far secure sex. Smiling cousins warrant law explain for whether.
     `*/
-let textPrompts = [
+/*let textPrompts = [
     'Welcome weary traveler...',
     "It's unexpected to receive visiters this time of year.",
     "Ah. But nonetheless it's a welcome visit.",
     "What may I do for you?",
+]*/
+let textPrompts = [
+    'yo',
 ]
 
-let dialogBox = document.querySelector('.dialogBox')
+function Chat(documentClassName){
+    this.dialogBox = document.querySelector(documentClassName)
+    this.dialogBox.innerHTML = ""
+    this.dialogBox.style.backgroundColor = "transparent";
+    this.dialogBox.style.color = "#cecee0";   
+    this.changeInnerText = changeInnerText;
+    this.timeout = timeout;
+    this.typeOutText = typeOutText;
+    this.typeOutMultiDialogs = typeOutMultiDialogs;
+    this.pressedPromptButton = pressedPromptButton;
+    this.userPrompts = userPrompts;
+}
+
+/*let dialogBox = document.querySelector('.dialogBox')
 dialogBox.innerHTML = ""
 dialogBox.style.backgroundColor = "transparent";   
-//dialogBox.style.height = "50px";   
-//dialogBox.style.width = "400px";   
 dialogBox.style.color = "#cecee0";   
-
+*/
 function changeInnerText(innerText, delay)
 {
     console.log({innerText,delay})
 
     return (new Promise(resolve => {
         setTimeout(() => {
-            dialogBox.innerHTML = innerText;
-            /*if (characterMovement === 0){
-                mainCharacterProfile.src = "robotCroppedFace6.png"
-                characterMovement = 1
-            }
-            else{
-                mainCharacterProfile.src = "robotCroppedFace5.png"
-                characterMovement = 0
-            }*/
+            this.dialogBox.innerHTML = innerText;
             resolve("resolved")
         }, delay)
     }))
 }
+
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -97,33 +95,85 @@ function timeout(ms) {
 async function typeOutText(dialog){
     //time to wait befor starting the next line of dialog passed in
     await timeout(1000)
-    dialogBox.innerHTML = ""
+    console.log("yo")
+    console.log(this)
+    this.dialogBox.innerHTML = ""
     let tempDialog = ""
     let i = 50
     for(let c of dialog){
         tempDialog = tempDialog + c
-        await changeInnerText(tempDialog, i)
+        await this.changeInnerText(tempDialog, i)
     }
     return 1;
 }
 
+async function pressedPromptButton(prompt){
+    console.log("pressedPromptButton")
+    responseButton1.classList.remove("RBActive")
+    await this.typeOutMultiDialogs(prompt, 0)
+    
+
+}
+
+function userPrompts(){
+    //Sets class that reveals buttons
+    responseButton1.classList.add("RBActive")
+    //Adds event handler that for questions
+    let whoAreYou = document.querySelector('.whoAreYou')
+    let aboutYourself = document.querySelector('.aboutYourself')
+    //Remove event listeners on click 
+    const clicked = async () => {
+        await this.pressedPromptButton(whoAreYouPrompt)
+        document.querySelector('.robotName').classList.add("robotNameKnown")
+
+        whoAreYou.removeEventListener("click", clicked, false);
+    }
+
+    whoAreYou.addEventListener("click", clicked, false);
+    
+
+}
  async function typeOutMultiDialogs(diagArry, delayBeforeTextSequence){
+    console.log("typeOutMultiDialogs")
+    console.log(this)
     await timeout(delayBeforeTextSequence)
     for (const diag of diagArry) {
         console.log(diag)
-        await typeOutText(diag);
+        await this.typeOutText(diag);
         console.log("finished type out text")
-        if (characterMovement === 1){
-            mainCharacterProfile.src = "robotCroppedFace5.png"
-            characterMovement = 0
-            console.log("Reset cropped face")
-        }
       }
       //Display user prompts after main character finished speaking
-      responseButton1.classList.add("RBActive")
+      console.log("check out here")
+      console.log(this.dialogBox.className)
+      //Populate and setup actions only after dialog box has finished from the robot
+      if(this.dialogBox.className == "robotDialogBox"){
+          this.userPrompts()
+      }
       console.log("finished noah")
 }
-typeOutMultiDialogs(textPrompts,3000)
+
+const mainCharacter = new CharacterProfile('.robotProfile', 'robotPx.png')
+const visitorCharacter = new CharacterProfile('.visitorProfileImg', 'visitorPx.png')
+
+const mainCharacterChat = new Chat('.robotDialogBox')
+mainCharacterChat.typeOutMultiDialogs(textPrompts, 3000)
+
+const visitorChat = new Chat('.visitorDialogBox')
+visitorChat.typeOutMultiDialogs(textPrompts, 3000)
+
+const middleSectionChat = new Chat('.middleSection')
+middleSectionChat.typeOutMultiDialogs(textPrompts, 3000)
+
+let whoAreYouPrompt = [
+    'My name...',
+    "In my past life I was known as Noah...",
+    "But I've been going by the alias Human3rr as long as I can remember",
+    "Any other questions?"
+]
+
+
+//typeOutMultiDialogs(textPrompts,3000)
+
 //wrap this call in another function.
 //After dialog is finished, create response options
 
@@ -143,5 +193,5 @@ typeOutMultiDialogs(textPrompts,3000)
 for(let c of dialog){
     console.log(c)
 }*/
-console.log(dialogBox.style)
+//console.log(dialogBox.style)
 
